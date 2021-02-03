ee74c63748edf5ea108ac58ce9cf10354a049085
Much4puk <lubos.frajtko@centrum.sk>
2021-02-03 21:26:35 +0100

    nastyloval som main page

---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('content')
+    
+    <h3><span>Dnes je pondelok 1.1.2021</span></h3>
+    <h2>Kto má dnes meniny?</h2>
+    <div class="border"></div>
+    <h1>Ľuboš</h1>
+
+
+
+
+
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,25 +9,47 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Mrs+Saint+Delafield&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Miss+Fajardose&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ URL::asset('css/style.css')}}">
 
     </head>
    <body>
-        <header>
-            <div class="logo">
-                <h1>Nameday</h1>
-            </div>
-            <nav>
+        <div>
+            <header class="header row">
+                <div class="logo col-sm-7 text-right">
+                    <h1>Nameday</h1>
+                </div>
+                <nav class="menu col-sm-5 text-right">
+                    <a href="">Home</a>
+                    <input class="search" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
 
-            </nav>
-        </header>
-        <main>
-            @yield('content')
-        </main>
-        <footer>
+                    <!-- <ul id="myUL">
+                        <li><a href="#">Adele</a></li>
+                        <li><a href="#">Agnes</a></li>
+                    
+                        <li><a href="#">Billy</a></li>
+                        <li><a href="#">Bob</a></li>
+                    
+                        <li><a href="#">Calvin</a></li>
+                        <li><a href="#">Christina</a></li>
+                        <li><a href="#">Cindy</a></li>
+                    </ul> -->
 
-        </footer>
+                </nav>
+            </header>
+            <main class="content">
+                <div class="container">
+                    @yield('content')
+                </div>
+            </main>
+            <footer class="footer">
+                <a href=""> 2021. <span>Vytvoril Ľuboš Frajtko.</span></a>
+            </footer>
+        </div>
+
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="{{ URL::asset('js/script.js')}}"></script>
 
    </body>
 </html>
