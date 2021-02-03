ea5c04b255cb6ea597483af151249e562157ee6b
Much4puk <lubos.frajtko@centrum.sk>
2021-02-03 19:31:55 +0100

    pridal som font family a logo

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,18 +8,18 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Mrs+Saint+Delafield&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="">
-        <style>
-            body {
-                font-family: 'Nunito';
-            }
-        </style>
+        <link rel="stylesheet" href="{{ URL::asset('css/style.css')}}">
+
     </head>
    <body>
         <header>
+            <div class="logo">
+                <h1>Nameday</h1>
+            </div>
             <nav>
-                
+
             </nav>
         </header>
         <main>
